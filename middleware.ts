50eb107b4e00573150417971c2f12389ed12d47b
gajay <devakm95@gmail.com>
2024-08-28 12:03:03 +0530

    redirect

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,28 @@
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import type { NextRequest } from 'next/server';
+
 export function getSession() {
     const session = cookies().get('session')?.value;
     return session ? session : null;
-  }
+}
 
 export function middleware(request: NextRequest) {
-  const session = getSession();
-  if (session && request.nextUrl.pathname.startsWith('/signup')) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+    const session = getSession();
 
-  if (session && request.nextUrl.pathname.startsWith('/signin')) {
-    return NextResponse.rewrite(new URL('/', request.url));
-  }
-
-  if (!session && !request.nextUrl.pathname.startsWith('/signin')) {
-    return NextResponse.rewrite(new URL('/signin', request.url));
-  }
+    // If there is a session, redirect to the root '/'
+    if (session) {
+        if (request.nextUrl.pathname !== '/') {
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+    } else {
+        // If there is no session, redirect to either '/signin' or '/signup'
+        if (!request.nextUrl.pathname.startsWith('/signin') && !request.nextUrl.pathname.startsWith('/signup')) {
+            return NextResponse.redirect(new URL('/signin', request.url));
+        }
+    }
 }
+
 export const config = {
-  matcher: ['/', '/profile', '/signin', '/signup'],
+    matcher: ['/', '/signin', '/signup'],
 };
